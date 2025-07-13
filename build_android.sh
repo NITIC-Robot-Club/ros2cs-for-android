4214d743de280bf368ce5cf375a572899ec59e1d
saku-1192 <sakusaku061110@gmail.com>
2025-07-14 02:47:21 +0900

    FIx build error

--- build_android.sh
+++ build_android.sh
@@ -74,6 +74,9 @@ ROS2CS_CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release \
 -DBUILD_TESTING=OFF \
 -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,'\$ORIGIN',-rpath=.,--disable-new-dtags" \
 -DCMAKE_FIND_ROOT_PATH=${PWD}/install/ \
+-DPython3_EXECUTABLE=/usr/bin/python3.12 \
+-DPython3_INCLUDE_DIR=/usr/include/python3.12 \
+-DPython3_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.12.so \
 --no-warn-unused-cli \
 -Wno-deprecated \
 -Wno-pointer-bool-conversion"
